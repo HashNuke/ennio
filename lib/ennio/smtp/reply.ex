c00cf87f7ee667cc964ff6b09bbd9ef7b664acb6
Akash Manohar J <akash@akash.im>
2015-12-11 21:19:26 +0530

    Reply module

--- lib/ennio/smtp/reply.ex
+++ lib/ennio/smtp/reply.ex
@@ -1,39 +1,50 @@
 defmodule Ennio.Smtp.Reply do
 
+  alias Ennio.SmtpConnection
+  alias Ennio.Config
+
+
   def ok(conn) do
-    "220 ok"
+    SmtpConnection.output conn, "250 ok"
   end
 
-  def complete(data) do
-    "250-#{data}"
+
+  def success(conn, data) do
+    SmtpConnection.output conn, "250-#{data}"
   end
 
-  def complete(data, last: true) do
-    "250 #{data}"
+  def success(conn, data, last: true) do
+    SmtpConnection.output conn, "250 #{data}"
   end
 
-  def complete(data, multiline: true) do
-    count = Enum.count data
-    Enum.slice(data, 0, count - 1)
-    |> Enum.map(fn(x)-> complete(x) end)
+  def success(conn, data, multiline: true) do
+    line_count = Enum.count data
 
-    Enum.at(data, count - 1)
-    |> complete(last: true)
+    Enum.slice(data, 0, line_count - 1)
+    |> Enum.map(fn(x)-> success(conn, x) end)
+
+    last_line = Enum.at(data, line_count - 1)
+    success(conn, last_line, last: true)
   end
 
-  def error(:invalid_command_sequence) do
+
+  def error(conn, :bad_command_sequence) do
+    require Logger
+    Logger.info "invalid sequence"
+    SmtpConnection.output conn, "503 bad sequence of commands"
+  end
+
+  def error(conn, :bad_command) do
+    require Logger
+    Logger.info "invalid command"
+    SmtpConnection.output conn, "503 bad command"
+  end
+
+  def error(conn, :invalid_recipient_syntax) do
     #TODO
   end
 
-  def error(:invalid_command) do
-    #TODO
-  end
-
-  def error(:invalid_recipient_syntax) do
-    #TODO
-  end
-
-  def error(:invalid_from_syntax) do
+  def error(conn, :invalid_from_syntax) do
     #TODO
   end
 end
