cf9a6d4605dfaa45feeb58b16a37577e0ce5589d
Akash Manohar J <akash@akash.im>
2015-12-12 00:35:46 +0530

    Implement size as an extension

---
+++ lib/ennio/extensions/size.ex
@@ -0,0 +1,18 @@
+defmodule Ennio.Extensions.Size do
+  def init do
+    %{
+      hooks: [:after_data]
+    }
+  end
+
+
+  def name do
+    "SIZE #{Ennio.Config.max_size}"
+  end
+
+
+  def call(conn, data) do
+    #TODO
+  end
+
+end
