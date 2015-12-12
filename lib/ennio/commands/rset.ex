f7bc3ba0996ca860f6445a40d08a9fa6d9f0d758
Akash Manohar J <akash@akash.im>
2015-12-12 22:55:16 +0530

    Renegotiate ssl if secure socket

--- lib/ennio/commands/rset.ex
+++ lib/ennio/commands/rset.ex
@@ -1,11 +1,14 @@
 defmodule Ennio.Commands.Rset do
-
   alias Ennio.Reply
 
   def name, do: "RSET"
 
   def call(conn, _args) do
-    conn = %Ennio.Connection{conn | mail: %Ennio.Mail{} }
+    if conn.secure do
+      :ok = :ssl.renegotiate conn.socket
+    end
+
+    conn = %Ennio.Connection{conn | mail: %Ennio.Mail{}}
     Reply.ok conn
     {:ok, conn}
   end
