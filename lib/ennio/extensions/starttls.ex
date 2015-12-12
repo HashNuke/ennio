04b34c738f68f3b4bb5b8a14ef7e96b251822e7a
Akash Manohar J <akash@akash.im>
2015-12-12 15:34:15 +0530

    Remove commands and such to cleanup starttls extension

--- lib/ennio/extensions/starttls.ex
+++ lib/ennio/extensions/starttls.ex
@@ -2,6 +2,8 @@ defmodule Ennio.Extensions.StartTls do
   require Logger
   alias Ennio.Reply
 
+  def name, do: "STARTTLS"
+
   def init do
     %{
       hooks: [:after_data],
@@ -10,26 +12,14 @@ defmodule Ennio.Extensions.StartTls do
   end
 
 
-  def name, do: "STARTTLS"
-
-
   def call(conn, data) do
-    Logger.debug "#{name} received"
-
-    # try moving this after the ssl_accept
     :ok = conn.transport.setopts conn.socket, [active: false]
-
     Reply.init conn
-    Logger.debug "SSL Accepting..."
     {:ok, ssl_socket} = :ssl.ssl_accept conn.socket, Ennio.Config.smtp[:ssl_opts]
 
-    # :ok = conn.transport.setopts conn.socket, [active: :once]
-
-    Logger.debug "Accepted SSL connection"
     conn = %Ennio.Connection{conn | socket: ssl_socket}
     |> Map.put(:secure, true)
 
-    # :ok = :ssl.controlling_process(conn.socket, self)
     :ok = :ssl.setopts conn.socket, [active: :once]
     {:ok, conn}
   end
