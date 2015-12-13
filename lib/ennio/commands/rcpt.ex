0f8df83b491b653d2b6c00e2e4ede79986783f9d
Akash Manohar J <akash@akash.im>
2015-12-13 20:12:56 +0530

    MAIL and RCPT commands complete

--- lib/ennio/commands/rcpt.ex
+++ lib/ennio/commands/rcpt.ex
@@ -1,28 +1,29 @@
-defmodule Ennio.Commands.Mail do
+defmodule Ennio.Commands.Rcpt do
   alias Ennio.Reply
 
-  def name, do: "MAIL"
+  def name, do: "RCPT"
 
 
   def call(conn, args) do
     case conn.mail do
       nil ->
-        parse_and_assign_from_email(conn, args)
-      _ ->
         Reply.error(conn, :invalid_command_sequence)
         {:ok, conn}
+      _ ->
+        parse_and_assign_to_email(conn, args)
     end
+    {:ok, conn}
   end
 
 
-  defp parse_and_assign_from_email(conn, args) do
-    case Ennio.AddressParser.find_email("from", args) do
+  defp parse_and_assign_to_email(conn, args) do
+    case Ennio.AddressParser.find_email("to", args) do
       :invalid_parameter_syntax ->
         Reply.error(conn, :invalid_parameter_syntax)
       :invalid_mailbox_syntax ->
         Reply.error(conn, :invalid_mailbox_syntax)
-      from_email ->
-        mail = %Ennio.Mail{from: from_email}
+      email_address ->
+        mail = %Ennio.Mail{from: email_address}
         conn = %Ennio.Connection{conn | mail: mail}
     end
     {:ok, conn}
